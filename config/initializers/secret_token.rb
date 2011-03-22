b0755fb35c21f00ffe67a2f24c12ea58cf8d147c
Shahn Miller <shahnmiller@yahoo.com>
2011-03-22 13:54:00 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalTest::Application.config.secret_token = '1601f35cdf110844488cdb44a253cbf6271605c96a5b83e44ef7a167a640a0e027d3763dacdba7fa4781246d97256cc01ab5b1ac34ef5a522f0f534a293f17fb'
